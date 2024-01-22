d201cfac8ac8de23760b55b6a311896cff20e2de
Jokast38 <jokast38@gmail.com>
2024-01-22 15:33:05 +0100

    home page correct
    correction certification practicum

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const axios = require('axios');
 const pug = require('pug');
 
 const app = express();
+app.set('view engine', 'pug'); // Set the view engine to use Pug
+
+const compiledFunction = pug.compileFile('./views/homepage.pug');
 
 const private_app_token = 'pat-eu1-509b02b8-801c-45ee-b4dc-a2d3db0a59cb';
 
-app.get('/', async (req, res) => {
+app.get('/homepage', async (req, res) => {
     const contactsEndpoint = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
         Authorization: `Bearer ${private_app_token}`,
@@ -16,6 +19,7 @@ app.get('/', async (req, res) => {
         const response = await axios.get(contactsEndpoint, { headers });
         const contactsData = response.data;
 
+        console.log(contactsData);
         res.render('homepage', { title: 'Hubspot Test', message: 'Banby', contacts: contactsData });
     } catch (error) {
         console.error(error);
@@ -23,4 +27,4 @@ app.get('/', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Listenning on http://localhost:3000'));
+app.listen(3000, () => console.log('Listening on http://localhost:3000'));
